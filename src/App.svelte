88567672b5052e666da37a3474fef7ceb422c90b
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-18 10:55:12 +0800

    Lesson 18: Starting the Polls Project

--- src/App.svelte
+++ src/App.svelte
@@ -1,78 +1,13 @@
 <script>
-  import Modal         from './Modal.svelte';
-  import AddPersonForm from './AddPersonForm.svelte';
 
-  let showModal = false;
-
-  let people = [
-    { name: 'yoshi', beltColour: 'black' , age: 25, id: 1 },
-    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
-    { name: 'luigi', beltColour: 'brown' , age: 35, id: 3 },
-  ];
-
-  const handleClick = (id) => {
-    // delete person from people
-    people = people.filter((person) => person.id !== id);
-  }
-
-  const toggleModal = () => {
-    showModal = !showModal;
-  };
-
-  const addPerson = (e) => {
-    const person = e.detail;
-    people    = [person, ...people];
-    showModal = false;
-  }
 </script>
 
-<Modal
-  {showModal}
-  on:click={toggleModal}
->
-  <AddPersonForm on:addPerson={addPerson}/>
-</Modal>
 
 <main>
-  <button on:click={toggleModal}>Open Modal</button>
-  {#each people as person (person.id)}
-    <div>
-      <h4>{person.name}</h4>
-      {#if person.beltColour === 'black'}
-        <p><strong>MASTER NINJA</strong></p>
-      {/if}
-      <p>{person.age} years old, {person.beltColour} belt.</p>
-      {#if person.skills}
-        <p><i>{person.skills.join(', ')}</i></p>
-      {/if}
-      <button on:click={() => handleClick(person.id)}>
-        delete
-      </button>
-      <hr>
-    </div>
-  {:else}
-    <p>There are not people to show...</p>
-  {/each}
+    <h1>Hello, Ninjas</h1>
 </main>
 
+
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
