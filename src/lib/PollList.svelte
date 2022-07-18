95a159b4c19223d3d134957be51c0f62cc747da3
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-18 14:32:57 +0800

    Lesson 30: Lifecycle Hooks

--- src/lib/PollList.svelte
+++ src/lib/PollList.svelte
@@ -1,18 +1,30 @@
 <script>
+    // import {onMount, onDestroy} from 'svelte';
     import PollStore   from '../stores/PollStore.js'
     import PollDetails from './PollDetails.svelte';
 
     export let polls = [];
 
-    PollStore.subscribe((data) => {
+    const unsub = PollStore.subscribe((data) => {
         polls = data;
     });
+
+    // // lifecycle hooks
+    // onMount(() => {
+    //     // maybe get data from the database
+    //     console.log('component mounted');
+    // });
+    // onDestroy(() => {
+    //     // unsubscribe from the store
+    //     console.log('component destroyed');
+    //     unsub();
+    // });
 </script>
 
 
 <div class="container">
     <div class="poll-list">
-        {#each polls as poll (poll.id)}
+        {#each $PollStore as poll (poll.id)}
             <div>
                 <PollDetails {poll} on:vote/>
             </div>
