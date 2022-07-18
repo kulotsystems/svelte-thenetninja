4d31b5c061a9f96333270734676a422c186e4fe2
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-18 10:15:28 +0800

    Lesson 16: Forms 2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -3,8 +3,15 @@
     let beltColour;
     let age;
 
+    // let fighting = false;
+    // let sneaking = false;
+    // let running = false;
+
+    let skills = [];
+
     const handleSubmit = () => {
         console.log(name, beltColour, age);
+        console.log(skills);
     }
 </script>
 
@@ -15,15 +22,26 @@
         bind:value={name}
     />
     <input
-        type="text"
-        placeholder="belt colour"
-        bind:value={beltColour}
-    />
-    <input
         type="number"
         placeholder="age"
         bind:value={age}
     />
+    <label>Skills</label>
+    <!--<label><input type="checkbox" bind:checked={fighting}></label> fighting <br>-->
+    <!--<label><input type="checkbox" bind:checked={sneaking}></label> sneaking <br>-->
+    <!--<label><input type="checkbox" bind:checked={running}></label>  running  <br>-->
+
+    <label><input type="checkbox" bind:group={skills} value="fighting"></label> fighting <br>
+    <label><input type="checkbox" bind:group={skills} value="sneaking"></label> sneaking <br>
+    <label><input type="checkbox" bind:group={skills} value="running"></label>  running  <br>
+
+    <label>Belt colour:</label>
+    <select bind:value={beltColour}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="brown">brown</option>
+        <option value="white">white</option>
+    </select>
     <button>Add Person</button>
 </form>
 
